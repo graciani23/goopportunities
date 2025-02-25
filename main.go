33921ba7906624c98f7ccaa2be024baa4173fe24
graciani23 <gracianigss@gmail.com>
2025-02-25 11:11:56 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"github.com/graciani23/goopportunities/config"
 	"github.com/graciani23/goopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errf("Error initializing configs: %v", err)
+		return
+	}
 	// initialize the gin router
 	router.Init()
 }

0062c31726885668ab1ac45fce4e9db48cc02f12
graciani23 <gracianigss@gmail.com>
2025-02-27 13:44:20 -0300

    feat: creating handlers

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/graciani23/goopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
